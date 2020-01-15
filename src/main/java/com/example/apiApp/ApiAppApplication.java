231dc8e40766d287858da95f22e3ff64162de8a7
野村 俊太 <nomurashunta@LAB-N1156.local>
2020-01-15 18:08:16 +0900

    Initial commit

---
+++ src/main/java/com/example/apiApp/ApiAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.apiApp;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apiApp/ApiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
